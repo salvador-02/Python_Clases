5417ec7415fa111489cef6259bf224da46bf1e52
salvador-02 <salvadormastandre@gmail.com>
2021-08-31 09:35:37 -0300

    Modificando

--- Clase_01/Introduccion.py
+++ Clase_01/Introduccion.py
@@ -1,1 +1,19 @@
+#Strings = 
 print ("Hola Mundo")
+print ( " Hola Mundo ")
+print ("""Hola Mundo""")
+print ("Pedro" + "Vive" + "En" + "La" + "Calle" + 2020)
+
+
+#Integrer = 
+print(2021)
+
+
+#Float
+print(5.3)
+
+
+#Booleano
+true = 1
+false = 0 
+
