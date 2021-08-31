b7070fcd8e63f1649c42c5b2fa445be6420ba925
salvador-02 <salvadormastandre@gmail.com>
2021-08-31 09:43:48 -0300

    cambios

--- Clase_01/Introduccion.py
+++ Clase_01/Introduccion.py
@@ -5,7 +5,7 @@ print ("""Hola Mundo""")
 print ("Pedro" + "Vive" + "En" + "La" + "Calle" + 2020)
 
 
-#Integrer = 
+#Integrer 
 print(2021)
 
 
@@ -16,4 +16,17 @@ print(5.3)
 #Booleano
 true = 1
 false = 0 
+
+#List = Grupa datos en corchetes 
+#La posicion inicial siempre es 0
+
+List_A = [0, 50, 60, 80, 90]
+List_B = ["Hola", "Mundo"]
+List_C = ["Hola", 30, true, 5.4]
+List_D = []
+List_E = {"Tambien funcionan estos corchetes"}
+
+#Tupla = Constante fija 
+t = (0,1 ,2 ,3 ,4 ,5)
+
 
