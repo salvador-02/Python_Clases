8de7fe4a4edd0085fa95cdb855ce6a8a570f77f2
salvador-02 <salvadormastandre@gmail.com>
2021-08-31 10:22:57 -0300

    cambios

--- Clase_01/Introduccion.py
+++ Clase_01/Introduccion.py
@@ -1,17 +1,14 @@
 #Strings = 
-print ("Hola Mundo")
-print ( " Hola Mundo ")
-print ("""Hola Mundo""")
-print ("Pedro" + "Vive" + "En" + "La" + "Calle" + 2020)
-
+print ("Hola Mundo");
+print ( " Hola Mundo ");
+print ("""Hola Mundo""");
+print ("Pedro" + "Vive" + "En" + "La" + "Calle" + 2020);
 
 #Integrer 
-print(2021)
-
+print(2021);
 
 #Float
-print(5.3)
-
+print(5.3);
 
 #Booleano
 true = 1
@@ -19,14 +16,47 @@ false = 0
 
 #List = Grupa datos en corchetes 
 #La posicion inicial siempre es 0
-
-List_A = [0, 50, 60, 80, 90]
-List_B = ["Hola", "Mundo"]
-List_C = ["Hola", 30, true, 5.4]
-List_D = []
-List_E = {"Tambien funcionan estos corchetes"}
+List_A = [0, 50, 60, 80, 90];
+List_B = ["Hola", "Mundo"];
+List_C = ["Hola", 30, true, 5.4];
+List_D = [];
+List_E = {"Tambien funcionan estos corchetes"};
 
 #Tupla = Constante fija 
-t = (0,1 ,2 ,3 ,4 ,5)
+a = (0,1 ,2 ,3 ,4 ,5);
+b = ();
+
+#Diccionarios = Objetos Literales 
+{"Nombre":"Salvador", "Mastandrea":
+print(type
+)
+}
+
+#None
+type(None);
+
+#Variables
+Nombre = ("Salvador")
+print(Nombre)
+
+#Key Sensitive 
+#Si la variable declarada no posee mayuscula y se escribe como tal, no es onsiderado el mismo tipo de variable 
+Libro = (23,x) = "Timepo Libre"
+print(x,Libro) = ("Timepo Libre", 23)
+
+#Constante
+T = "Hola"
+
+#Reasignacion de variables 
+MyLibro = "El_Principito"
+MyLibro = ""
+
+#Strings 
+myStr = "Hola Mundo"
+dir() = "Lo Que Se Puede Hacer Con Este String"
+upper = ("Convierte todo en mayuscula")
+capitalize = ("Funcion que convierte la primera letra en ayuscula")
+print(myStr.index("a")) = "Muestra La Cantidad De Letras"
+
 
 
---
+++ Clase_01/Practica.py
@@ -0,0 +1,19 @@
+#ClasePhyton
+
+print("Hola Mundo")
+print("""Hola Mundo""")
+
+print([30,50,10,40,60])
+print(["Hola", "Mundo"])
+print([30, "Hola", False, 5.4])
+print([])
+
+print (type ([30, "Hola", False, 5.4]))
+print(type(None))
+
+myStr = "Hola Mundo"
+
+print(len(myStr))
+
+print(myStr.index("a"))
+print(dir(myStr))
