3b9b713f1c890189e902e9cc456a71b2a6aab61d
salvador-02 <salvadormastandre@gmail.com>
2021-09-01 09:08:24 -0300

    commit

--- Clase_01/Tarea.py
+++ Clase_01/Tarea.py
@@ -1,13 +1,17 @@
 #dado dos datos de alturas de dos diferentes personas, decir cual es la mayor, devolver el nombre y la altura de las dos
 
-Persona1 = {"Nombre" : "Angel" , "Altura" : "1.98"}
-Persona2 = {"Nombre" : "Luis" , "Altura" : "1.90"}
+Persona1 = {"Nombre" : "Angel" , "Altura" : "1.85"}
+Persona2 = {"Nombre" : "Luis" , "Altura" : "1.78"}
 
-if Persona1.get("Altura") >= Persona2.get("Altura"):
+if Persona1.get("Altura") == Persona2.get("Altura"):
+    print(Persona1.get("Nombre"),"mide", Persona1.get("Altura"), "y", Persona2.get("Nombre")
+    , "mide", Persona2.get("Altura"), "por ende, miden lo mismo")
+
+if Persona1.get("Altura") > Persona2.get("Altura"):
     print(Persona1.get("Nombre"),"es mas alto porque mide", Persona1.get("Altura"), "encambio", Persona2.get("Nombre")
     , "mide", Persona2.get("Altura"))
 else:
-    print(Persona2.get("Nombre"), "es mas alto poque mide", Persona2.get("Altura"), "minetras que", Persona1.get("Nombre"), "mide"
+    print(Persona2.get("Nombre"), "es mas alto porque mide", Persona2.get("Altura"), "minetras que", Persona1.get("Nombre"), "mide"
     , Persona1.get("Altura"))
 
 
--- Clase_02/Ejercicios.py
+++ Clase_02/Ejercicios.py
@@ -1,3 +1,4 @@
+#Ej.1
 # Hacer un programa que introduzca el usuario dos palabras y detectar si es un anagrama.
 
 Palabra1 = ("Sapo")
@@ -9,9 +10,6 @@ Palabra2_list = list(Palabra2)
 Palabra1_list.sort()
 Palabra2_list.sort()
 
-Palabra1_list = "".join(Palabra1_list)
-Palabra2_list = "".join(Palabra2_list)
-
 if Palabra1_list == Palabra2_list:
     print(Palabra1, "y", Palabra2, "son un anagrama")
 else:
@@ -19,6 +17,7 @@ else:
 
 
 
+#Ej.2
 # Realizar un programa para introducir un mail  y  verificar si esta correcto o no. 
 
 mail = ("salvdor.mastandrea@comunidad.ub.edu.ar")
@@ -30,7 +29,8 @@ else:
 
 
 
-# hacer un programa que el usurario escriba una frase y detectar si existe la palabra argentina.
+#Ej.3
+# Hacer un programa que el usurario escriba una frase y detectar si existe la palabra argentina.
 
 Frase = ("frase Argentina")
 
@@ -41,17 +41,20 @@ else:
 
 
 
+#Ej.4
 # Realizar un programa que me devuelva el área de un rectángulo, introduciendo por teclado base y altura -🡪  area = base * altura
 
 Base = 4
 Altura = 5
 Area = (Base * Altura)
-print(Area)
+print("El area del rectangulo es",Area)
 
 
+#Ej.5
 # Realizar un programa que me devuelva la superficie del círculo, introduciendo por teclado radio. Definir la constante PI.
 
 import math
 Radio = 5
 area = math.pi*Radio**2
-print(area)
+print("La superficie del circulo es", area)
+
